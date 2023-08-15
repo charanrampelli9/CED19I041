ee29a944ffe78f5b40a4ed19d79c9b01031d1084
Rampelli Charan Kumar <rampellicharankumar02@gmail.com>
2023-08-15 16:05:37 +0530

    last commit

--- Q1/app.py
+++ Q1/app.py
@@ -1,28 +1,33 @@
 #CED19I041
 #Charan Kumar 
 from flask import Flask, request, jsonify
-import requests
-import json
+import asyncio
+import aiohttp
 
 app = Flask(__name__)
 
+async def fetch_numbers(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                data = await response.json()
+                return data.get("numbers", [])
+            return []
+
+
 @app.route('/numbers', methods=['GET'])
-def get_numbers():
+async def get_numbers():
     urls = request.args.getlist('url')
+    tasks = [fetch_numbers(url) for url in urls]
 
-    result = []
-    for url in urls:
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                data = response.json()
-                result.extend(data)
-            else:
-                result.append({'error': f'Failed to fetch data from {url}', 'url': url})
-        except Exception as e:
-            result.append({'error': str(e), 'url': url})
+    try:
+        results = await asyncio.gather(*tasks)
+    except asyncio.TimeoutError:
+        results = []
 
-    return jsonify(result)
+    merged_numbers = sorted(set(number for result in results if isinstance(result, list) for number in result))
+    
+    return jsonify({"numbers": merged_numbers})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8008,debug=True)
